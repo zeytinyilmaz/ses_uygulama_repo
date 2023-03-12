a656929e2c687fa70fd4a437b759e45c310bdf82
pentest <muratzeytin1997@hotmail.com>
2023-03-12 19:30:48 +0300

    Notalar alınabiliyor ve ses dosyası okunup wav dosyasına kaydedilebiliyor. ilerleyen süreçte wav dataları sabit hale getiricez.

--- test.py
+++ test.py
@@ -22,7 +22,18 @@ birlestir.export('/home/pentest/Masaüstü/sesdeneme.wav', format='wav', tags={'
 
 sound = AudioSegment.from_file("/home/pentest/Masaüstü/assets/notes/Gb7.wav", format="wav")
 print(sound.frame_rate)
+from pydub.utils import mediainfo
 
-ses1 = Sine(sound.frame_rate).to_audio_segment(duration=saniye1)
-ses1.export('/home/pentest/Masaüstü/sesdeneme.wav', format='wav', tags={'artist': 'project', 'album': 'project', 'comments': 'project'})
-print('bitti')
+#ses1 = Sine(sound.frame_rate).to_audio_segment(duration=saniye1)
+#ses1.dBFS = sound.dBFS
+#ses1.frame_rate = sound.frame_rate
+#ses1.frame_width = sound.frame_width
+#ses1.channels = sound.channels
+#ses1.DEFAULT_CODECS = sound.DEFAULT_CODECS
+#ses1.export('/home/pentest/Masaüstü/sesdeneme.wav', format='wav', tags={'artist': 'project', 'album': 'project', 'comments': 'project'})
+#print('bitti')
+
+from pydub.playback import play
+new_wav_file = AudioSegment.from_file(file="/home/pentest/Masaüstü/assets/notes/Gb7.wav", format='wav')
+yeni =  new_wav_file
+yeni.export('/home/pentest/Masaüstü/sesdeneme.wav', format='wav', tags={'artist': 'project', 'album': 'project', 'comments': 'project'})
